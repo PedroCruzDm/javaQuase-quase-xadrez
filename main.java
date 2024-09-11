d73eae55a35068b256732039721e97e80375c75c
O Famosoo Pedro <108557012+PedroCruzDm@users.noreply.github.com>
2024-09-10 21:26:49 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package xadrez;
+
+/**
+ *
+ * @author Admin
+ */
+public class main {
+    public static void main(String[] args) {
+    peao peaozin = new peao();
+    bispo bispinho = new bispo();
+    cavalo vagalo = new cavalo();
+    
+    
+    peaozin.mover();
+    bispinho.mover();
+    vagalo.mover();
+    
+    
+    }
+    
+    
+}
